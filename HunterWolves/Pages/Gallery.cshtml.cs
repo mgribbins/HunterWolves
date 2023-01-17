b9816f38da45236b393b8b7d41179428048ba9b0
Michael Gribbins <mgribbins@Hunter.com>
2023-01-17 08:25:12 -0600

    Add project files.

---
+++ HunterWolves/Pages/Gallery.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HunterWolves.Pages
+{
+    public class GalleryModel : PageModel
+    {
+        private readonly ILogger<GalleryModel> _logger;
+
+        public GalleryModel(ILogger<GalleryModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ HunterWolves/Pages/Thanks.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HunterWolves.Pages
+{
+    public class ThanksModel : PageModel
+    {
+        private readonly ILogger<ThanksModel> _logger;
+
+        public ThanksModel(ILogger<ThanksModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
